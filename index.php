dcae7863784dcc07f2736cc26c7734931a176967
muriloandrade1 <murilo.andrade1@gmail.com>
2016-09-11 20:09:57 +0800

    agora sim problema do botão bootstrap resolvido.

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     </div>
 </div>
 </header>
-
 <section id="content" class="main-content">
     <div class="container">
         <div class="row">
@@ -21,32 +20,8 @@
                             </div>  
                             <div class="thumbnail-pad">
                                 <p>Planejar é propor o melhor método estratégico, para entregar um produto de qualidade e no prazo que a sociedade deseja, visando melhorar a vida de todos.</p><br>
-                                <div class="container">
-                                    <!--<button class="btn btn_" data-toggle="modal" data-target="#myModal1">Saiba mais</button>-->
-                                    <span><a data-toggle="modal" data-target="#myModal1" class="btn btn_">Saiba mais</a></span>
-                                    <!-- Modal -->
-                                    <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
-                                        <div class="modal-dialog">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only"></span></button>
-                                                    <p class="lead">Planejamento</p>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <div class="panel-body">
-                                                        <p>Nossa empresa, com seu quadro de profissionais altamente qualificados, possui expertise em planejamento, seja estratégico ou executivo de grandes obras, tais como: Ferrovias, Aeroportos, Hidrelétricas e Obras de Artes Especiais.</p>
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <a href="servicos.php" class="btn btn_">Todos</a><a href="index.php" class="btn btn_">Sair</a>
-                                                        <!--<button type="button" class="btn btn_" data-dismiss="modal">Sair</button>-->
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <a data-toggle="modal" data-target="#myModal1" class="btn btn_">Saiba mais</a>
                             </div>
-                        </div>
                     </li>
                     <li class="span3">
                         <div class="thumbnail">
@@ -57,30 +32,8 @@
                             </div>  
                             <div class="thumbnail-pad">
                                 <p>Utilizamos as melhores práticas e softwares para o desenvolvimento de projetos de engenharia.</p><br><br>
-                                <div class="container">
-                                    <span><a data-toggle="modal" data-target="#myModal2" class="btn btn_">Saiba mais</a></span>
-                                    <!-- Modal -->
-                                    <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
-                                        <div class="modal-dialog">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only"></span></button>
-                                                    <p class="lead">Projetos de Engenharia</p>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <div class="panel-body">
-                                                        <p>Criamos e desenvolvemos projetos executivos, seja de arquitetura, estrutural, instalações prediais, proteção contra incêndio, etc. utilizando softwares de CAD 2D ou 3D e detalhamento técnico para um melhor entendimento na execução da obra.</p>
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <a href="servicos.php" class="btn btn_">Conheça todos</a><a href="index.php" class="btn btn_">Sair</a>
-                                                </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <a data-toggle="modal" data-target="#myModal2" class="btn btn_">Saiba mais</a>
                             </div>
-                        </div>
                     </li>
                     <li class="span3">
                         <div class="thumbnail">
@@ -90,31 +43,8 @@
                             </div>  
                             <div class="thumbnail-pad">
                                 <p>Gerenciamos e executamos obras com profissionais qualificados, tendo sempre como metas a satisfação do cliente, qualidade e prazo na entrega dos serviços.</p>
-                                <div class="container">
-                                    <span><a data-toggle="modal" data-target="#myModal3" class="btn btn_">Saiba mais</a></span>
-                                    <!-- Modal -->
-                                    <div class="modal fade" id="myModal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
-                                        <div class="modal-dialog">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only"></span></button>
-                                                    <p class="lead">Gerenciamento Fiscalização e Execução de Obras</p>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <div class="panel-body">
-                                                        <p>Gerenciamos, fiscalizamos e executamos obras comerciais e residenciais, oferecendo aos nossos clientes os melhores profissionais do mercado. Nossa visão é sempre utilizar produtos de qualidade, preocupação constante com o meio ambiente e obedecer aos prazos contratados.</p>
-                                                        <p>Gerenciamos e Fiscalizamos grandes obras civis além de executamos obras residenciais, reformas de apartamentos e condomínios.</p>
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <a href="servicos.php" class="btn btn_">Conheça todos</a><a href="index.php" class="btn btn_">Sair</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <a data-toggle="modal" data-target="#myModal3" class="btn btn_">Saiba mais</a>
                             </div>
-                        </div>
                     </li>
                     <li class="span3">
                         <div class="thumbnail">
@@ -125,34 +55,11 @@
                             </div>
                             <div class="thumbnail-pad">  
                                 <p>Elaboramos relatórios e laudos técnicos de engenharia, memoriais descritivos e Plano de Prevenção Contra Incêndio – PPCI para o CBM/DF.</p><br>
-                                <div class="container">
-                                    <!--<span><button style="width: 95px; height: 30px" class="btn btn_" data-toggle="modal" data-target="#myModal4">Saiba mais</button></span>-->
-                                    <span><a data-toggle="modal" data-target="#myModal4" class="btn btn_">Saiba mais</a></span>
-                                    <!-- Modal -->
-                                    <div class="modal fade" id="myModal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
-                                        <div class="modal-dialog">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only"></span></button>
-                                                    <p class="lead">Relatórios Técnicos, Laudos e Memoriais de Engenharia</p>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <div class="panel-body">
-                                                        <p>Fazemos visitas e vistorias técnicas objetivando a elaboração de laudos e relatórios técnicos e fotográficos. Preparamos memoriais descritivos de projetos de engenharia para melhor entendimento e aplicabilidade de Normas Técnicas.</p>
-                                                        <p>Além do Projeto de Proteção Contra Incêndio, elaboramos o Plano de Prevenção Contra Incêndio – PPCI, exigência do CBM/DF para diversos tipos de empreendimentos do DF.</p>
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                         <a href="servicos.php" class="btn btn_">Conheça todos</a><a href="index.php" class="btn btn_">Sair</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <a data-toggle="modal" data-target="#myModal4" class="btn btn_">Saiba mais</a>
                             </div>
-                        </div>
                     </li>
                 </ul>
+
             </div>
         </div>
         <div class="row">
@@ -165,7 +72,94 @@
                     </div>
                 </div>
             </div>    
-        </div>        
+        </div>    
     </div>
+    <div class="container">
+        <!-- Modal -->
+        <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only"></span></button>
+                        <p class="lead">Planejamento</p>
+                    </div>
+                    <div class="modal-body">
+                        <div class="panel-body">
+                            <p>Nossa empresa, com seu quadro de profissionais altamente qualificados, possui expertise em planejamento, seja estratégico ou executivo de grandes obras, tais como: Ferrovias, Aeroportos, Hidrelétricas e Obras de Artes Especiais.</p>
+                        </div>
+                        <div class="modal-footer">
+                            <!--<a href="servicos.php" class="btn btn_">todos</a>  <a class="btn btn_" href="index.php">Sair</a>-->
+                            <button type="button" class="btn btn_" data-dismiss="modal">Sair</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <!-- Modal -->
+        <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only"></span></button>
+                        <p class="lead">Projetos de Engenharia</p>
+                    </div>
+                    <div class="modal-body">
+                        <div class="panel-body">
+                            <p>Criamos e desenvolvemos projetos executivos, seja de arquitetura, estrutural, instalações prediais, proteção contra incêndio, etc. utilizando softwares de CAD 2D ou 3D e detalhamento técnico para um melhor entendimento na execução da obra.</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn_" data-dismiss="modal">Sair</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+    <div class="container">
+        <!-- Modal -->
+        <div class="modal fade" id="myModal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only"></span></button>
+                        <p class="lead">Gerenciamento Fiscalização e Execução de Obras</p>
+                    </div>
+                    <div class="modal-body">
+                        <div class="panel-body">
+                            <p>Gerenciamos, fiscalizamos e executamos obras comerciais e residenciais, oferecendo aos nossos clientes os melhores profissionais do mercado. Nossa visão é sempre utilizar produtos de qualidade, preocupação constante com o meio ambiente e obedecer aos prazos contratados.</p>
+                            <p>Gerenciamos e Fiscalizamos grandes obras civis além de executamos obras residenciais, reformas de apartamentos e condomínios.</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn_" data-dismiss="modal">Sair</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>   
+    <div class="container">
+        <!-- Modal -->
+        <div class="modal fade" id="myModal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only"></span></button>
+                        <p class="lead">Relatórios Técnicos, Laudos e Memoriais de Engenharia</p>
+                    </div>
+                    <div class="modal-body">
+                        <div class="panel-body">
+                            <p>Fazemos visitas e vistorias técnicas objetivando a elaboração de laudos e relatórios técnicos e fotográficos. Preparamos memoriais descritivos de projetos de engenharia para melhor entendimento e aplicabilidade de Normas Técnicas.</p>
+                            <p>Além do Projeto de Proteção Contra Incêndio, elaboramos o Plano de Prevenção Contra Incêndio – PPCI, exigência do CBM/DF para diversos tipos de empreendimentos do DF.</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn_" data-dismiss="modal">Sair</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
 </section>
 <?php include'footer.php'; ?>
